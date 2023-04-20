e5d46b4d713b54e1e55a7a87a0f1cd003d7f549d
+ <+>
2023-04-20 05:22:05 +0300

    completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,16 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import { trees, wildlife, elevation } from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
   return <h1>Colorado State Parks!</h1>;
 }
+//done
+
+console.log(trees);
+wildlife();
+elevation();
+
+export default ColoradoStateParks;
--- src/parks/MesaVerde.js
+++ src/parks/MesaVerde.js
@@ -1,7 +1,7 @@
 import React from "react";
+import { wildlife } from "./RockyMountain";
 
 function MesaVerde() {
   return <h1>Mesa Verde National Park</h1>;
 }
 
-export default MesaVerde;
